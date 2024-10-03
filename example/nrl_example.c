e4673eb3aac0dfd507f8045f686126250f094d17
Vladyslav Aviedov <vladaviedov@protonmail.com>
2024-10-03 15:11:43 -0400

    update example code

--- example/nrl_example.c
+++ example/nrl_example.c
@@ -7,8 +7,30 @@
 #include <nanorl/nanorl.h>
 
 int main(void) {
+	nrl_config config = nrl_default_config();
+	config.prompt = "enter something: ";
+
 	// Basic usage
-	char *input = nrl_readline("enter something: ");
+	char *input = nanorl(&config, NULL);
+	printf("You typed: %s\n\n", input);
+	free(input);
+
+	// Obscured
+	config.echo_mode = NRL_ECHO_OBSCURED;
+	input = nanorl(&config, NULL);
+	printf("You typed: %s\n\n", input);
+	free(input);
+
+	// No echo
+	config.echo_mode = NRL_ECHO_OFF;
+	input = nanorl(&config, NULL);
+	printf("You typed: %s\n\n", input);
+	free(input);
+
+	// Preload
+	config.echo_mode = NRL_ECHO_ON;
+	config.preload = "value";
+	input = nanorl(&config, NULL);
 	printf("You typed: %s\n\n", input);
 	free(input);
 
