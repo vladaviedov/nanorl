996d281e1881fd147a56ff2696adb8696bfdf357
Vladyslav Aviedov <vladaviedov@protonmail.com>
2024-10-04 22:21:42 -0400

    example: show error code

--- example/nrl_example.c
+++ example/nrl_example.c
@@ -6,24 +6,30 @@
 
 #include <nanorl/nanorl.h>
 
+const char *err_to_string(nrl_error err);
+
 int main(void) {
 	nrl_config config = nrl_default_config();
+	nrl_error error;
 	config.prompt = "enter something: ";
 
 	// Basic usage
-	char *input = nanorl(&config, NULL);
+	char *input = nanorl(&config, &error);
+	printf("%s\n", err_to_string(error));
 	printf("You typed: %s\n\n", input);
 	free(input);
 
 	// Obscured
 	config.echo_mode = NRL_ECHO_OBSCURED;
-	input = nanorl(&config, NULL);
+	input = nanorl(&config, &error);
+	printf("%s\n", err_to_string(error));
 	printf("You typed: %s\n\n", input);
 	free(input);
 
 	// No echo
 	config.echo_mode = NRL_ECHO_OFF;
-	input = nanorl(&config, NULL);
+	input = nanorl(&config, &error);
+	printf("%s\n", err_to_string(error));
 	printf("You typed: %s\n\n", input);
 	free(input);
 
@@ -31,9 +37,27 @@ int main(void) {
 	config.echo_mode = NRL_ECHO_ON;
 	config.prompt = "edit this text: ";
 	config.preload = "hello world";
-	input = nanorl(&config, NULL);
+	input = nanorl(&config, &error);
+	printf("%s\n", err_to_string(error));
 	printf("You typed: %s\n\n", input);
 	free(input);
 
 	return 0;
+}
+
+const char *err_to_string(nrl_error err) {
+	switch (err) {
+	case NRL_ERROR_OK:
+		return "Success!";
+	case NRL_ERROR_ARG:
+		return "Bad argument";
+	case NRL_ERROR_SYSTEM:
+		return "System error";
+	case NRL_ERROR_EOF:
+		return "EOF reached";
+	case NRL_ERROR_INTERRUPT:
+		return "Interrupted!";
+	}
+
+	return NULL;
 }
